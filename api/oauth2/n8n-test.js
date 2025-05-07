488ff41833eea2419c3bb2f001638d7e312245f5
Matthew Zienert <mzienert@gmail.com>
2025-05-06 18:15:07 -0600

    Clean up unnecessary serverless functions

--- api/oauth2/n8n-test.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-// Handler to serve the n8n test page
-module.exports = function handler(req, res) {
-  // Redirect to the new URL to avoid file conflicts
-  res.redirect(302, '/api/oauth2/n8n-oauth-test');
-} 

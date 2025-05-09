0ccf34640b2212a8fad980878992f149fbb81cb4
Matthew Zienert <mzienert@gmail.com>
2025-05-09 12:42:14 -0600

    start fresh

--- get-org.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Replace with your access token
-source .env
-
-curl --request GET \
-  --url https://api.calendly.com/users/me \
-  --header "Authorization: Bearer ${CALENDLY_TOKEN}" 
